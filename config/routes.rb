e8fc8c11907f6a3bb82d7347b0bd54604abc01c7
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-06-23 04:07:00 +0600

    Add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+ end
+  
   resources :clients
   #get 'home/index'
    get 'home/about'
